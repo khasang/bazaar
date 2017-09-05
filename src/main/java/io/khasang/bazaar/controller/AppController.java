652d7282ff0d3a768e719542994908ae6ca4953f
Keos99 <keos99@bk.ru>
2017-09-05 21:43:57 +0300

    New Commit

--- src/main/java/io/khasang/bazaar/controller/AppController.java
+++ src/main/java/io/khasang/bazaar/controller/AppController.java
@@ -1,4 +1,9 @@
 package io.khasang.bazaar.controller;
 
 public class AppController {
+    public static void main(String[] args) {
+        System.out.println("456");
+        System.out.println("654");
+        System.out.println("546");
+    }
 }

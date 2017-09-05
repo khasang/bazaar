1303b9cc25263c83e05de11085a23b4aae551161
Azon <azon.sk@gmail.com>
2017-09-05 20:44:25 +0300

    add diff kadabra

--- src/main/java/io/khasang/bazaar/controller/AppController.java
+++ src/main/java/io/khasang/bazaar/controller/AppController.java
@@ -1,4 +1,11 @@
 package io.khasang.bazaar.controller;
 
 public class AppController {
+
+    public static void main(String[] args) {
+        System.out.println("asddssadasda");
+        System.out.println("asddssa12312asda");
+        System.out.println("asddssad23asda");
+        System.out.println("asddssadasda");
+    }
 }

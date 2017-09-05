7a903b3ba36cef7f4724ed886c628752a987caf9
Azon <azon.sk@gmail.com>
2017-09-05 20:48:38 +0300

    fix text

--- src/main/java/io/khasang/bazaar/controller/AppController.java
+++ src/main/java/io/khasang/bazaar/controller/AppController.java
@@ -3,9 +3,6 @@ package io.khasang.bazaar.controller;
 public class AppController {
 
     public static void main(String[] args) {
-        System.out.println("asddssadasda");
-        System.out.println("asddssa12312asda");
-        System.out.println("asddssad23asda");
-        System.out.println("asddssadasda");
+        System.out.println("no need so many text");
     }
 }

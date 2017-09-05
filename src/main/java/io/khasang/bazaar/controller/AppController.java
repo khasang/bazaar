a8ca501ebf3ecc302c766c5e90e836c0b9800f60
Azon <azon.sk@gmail.com>
2017-09-05 20:43:50 +0300

    add abra kadabra

--- src/main/java/io/khasang/bazaar/controller/AppController.java
+++ src/main/java/io/khasang/bazaar/controller/AppController.java
@@ -1,7 +1,11 @@
 package io.khasang.bazaar.controller;
 
-public class AppController{
+public class AppController {
+
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("asdsad");
+        System.out.println("asds21");
+        System.out.println("as23sad");
+        System.out.println("asd23ad");
     }
 }

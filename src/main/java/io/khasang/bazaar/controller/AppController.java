a040cbd9ce7c7ba264e3dcf98bd6460d890fc1df
achubukov <sasha-chubucov@mail.ru>
2017-09-11 15:05:04 +0300

    test connect

--- src/main/java/io/khasang/bazaar/controller/AppController.java
+++ src/main/java/io/khasang/bazaar/controller/AppController.java
@@ -3,5 +3,6 @@ package io.khasang.bazaar.controller;
 public class AppController {
     public static void main(String[] args) {
         System.out.println("Branch achubukov");
+        System.out.println("Test");
     }
 }

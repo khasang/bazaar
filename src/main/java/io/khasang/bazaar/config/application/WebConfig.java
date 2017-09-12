43f11b7bf054ba4779319ade082726f64e1a0b44
Azon <azon.sk@gmail.com>
2017-09-12 21:01:02 +0300

    add basic structure

---
+++ src/main/java/io/khasang/bazaar/config/application/WebConfig.java
@@ -0,0 +1,24 @@
+package io.khasang.bazaar.config.application;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+// spring-dispatcher-servlet.xml
+@Configuration
+@EnableWebMvc
+@ComponentScan({"io.khasang.bazaar"})
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setContentType("text/html; charset=utf-8");
+        return viewResolver;
+    }
+}
---
+++ src/main/java/io/khasang/bazaar/model/Message.java
@@ -0,0 +1,23 @@
+package io.khasang.bazaar.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+    private String name;
+
+    public Message() {
+    }
+
+    public Message(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

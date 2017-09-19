fd2d86279a38d1de369a73a1d4b69b1b0f4bfdf7
Azon <azon.sk@gmail.com>
2017-09-19 20:44:01 +0300

    add spring security with DB auth

---
+++ src/main/java/io/khasang/bazaar/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package io.khasang.bazaar.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}

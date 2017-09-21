a9a1ab42d30c6fd30b782eeb470d0dbd6f21c6bc
Azon <azon.sk@gmail.com>
2017-09-21 21:13:07 +0300

    add hibernate config

---
+++ src/main/java/io/khasang/bazaar/entity/Cat.java
@@ -0,0 +1,62 @@
+package io.khasang.bazaar.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cats")
+public class Cat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cat_name")
+    private String name;
+    private String description;
+
+    public Cat() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+
+        if (id != null ? !id.equals(cat.id) : cat.id != null) return false;
+        if (name != null ? !name.equals(cat.name) : cat.name != null) return false;
+        return description != null ? description.equals(cat.description) : cat.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

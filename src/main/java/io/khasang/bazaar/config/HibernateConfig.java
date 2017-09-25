7be913f0324ad1a92071fada4a74f30726aa014c
achubukov <sasha-chubucov@mail.ru>
2017-09-25 13:22:18 +0300

    dz hibernate_v02

---
+++ src/main/java/io/khasang/bazaar/config/HibernateConfig.java
@@ -0,0 +1,71 @@
+package io.khasang.bazaar.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource(value = "classpath:hibernate.properties")
+public class HibernateConfig {
+
+    private final Environment environment;
+
+    @Autowired
+    public HibernateConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("io.khasang.bazaar.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("hibernate.driver"));
+        dataSource.setUrl(environment.getProperty("hibernate.url"));
+        dataSource.setUsername(environment.getProperty("hibernate.name"));
+        dataSource.setPassword(environment.getProperty("hibernate.password"));
+        return dataSource;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.setProperty("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+        properties.setProperty("show_sql", environment.getProperty("hibernate.show_sql"));
+        return properties;
+    }
+
+    @Bean
+    @Autowired
+    HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(sessionFactory);
+        return hibernateTransactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
---
+++ src/main/java/io/khasang/bazaar/entity/Cat.java
@@ -0,0 +1,67 @@
+package io.khasang.bazaar.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cats")
+public class Cat {
+
+    public Cat() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column (name = "cat_name")
+    String name;
+    String description;
+
+    @Transient
+    int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+
+        if (id != null ? !id.equals(cat.id) : cat.id != null) return false;
+        if (name != null ? !name.equals(cat.name) : cat.name != null) return false;
+        return description != null ? description.equals(cat.description) : cat.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}

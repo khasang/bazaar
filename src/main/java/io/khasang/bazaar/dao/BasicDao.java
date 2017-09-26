efc456753fd57ce3fd20decd95caeeb991a87a45
Azon <azon.sk@gmail.com>
2017-09-26 20:49:46 +0300

    add rest service

--- src/main/java/io/khasang/bazaar/dao/BasicDao.java
+++ src/main/java/io/khasang/bazaar/dao/BasicDao.java
@@ -1,5 +1,6 @@
 package io.khasang.bazaar.dao;
 
+import io.khasang.bazaar.entity.Cat;
 import org.hibernate.Session;
 
 import java.util.List;
@@ -7,22 +8,48 @@ import java.util.List;
 public interface BasicDao<T> {
 
     /**
+     * get current session
      *
-     * */
+     * @return session
+     */
     Session getCurrentSession();
 
     /**
+     * Receive entity by id
      *
-     * */
+     * @param id - entities id what we want to receive
+     * @return entity
+     */
     T getById(Long id);
 
     /**
+     * Create entity at database
      *
-     * */
+     * @param entity - entity for creation
+     * @return entity
+     */
     T addCat(T entity);
 
     /**
+     * Create entity at database
      *
-     * */
+     * @param entity - entity for creation
+     * @return entity
+     */
+    T updateCat(T entity);
+
+    /**
+     * Receive all entities from database
+     *
+     * @return all entities from database
+     */
     List<T> getList();
+
+    /**
+     * Delete entity from database
+     *
+     * @param entity - entity for deletion
+     * @return entity
+     */
+    T delete(T entity);
 }

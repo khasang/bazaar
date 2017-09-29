d6720a38644b76df07b7874db5da94f916938820
Azon <azon.sk@gmail.com>
2017-09-29 20:51:57 +0300

    add integration test

---
+++ src/test/java/io/khasang/bazaar/CatControllerIntegrationTest.java
@@ -0,0 +1,84 @@
+package io.khasang.bazaar;
+
+import io.khasang.bazaar.entity.Cat;
+import org.junit.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class CatControllerIntegrationTest {
+    private final String ROOT = "http://localhost:8080/cat";
+    private final String ADD = "/add";
+    private final String GET_BY_ID = "/get/id";
+    private final String GET_ALL = "/all";
+
+    @Test
+    public void addCat() {
+        Cat cat = createCat();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Cat> responseEntity = restTemplate.exchange(
+                ROOT + GET_BY_ID + "/{id}",
+                HttpMethod.GET,
+                null,
+                Cat.class,
+                cat.getId()
+        );
+        Cat receivedCat = responseEntity.getBody();
+        assertEquals("OK", responseEntity.getStatusCode().getReasonPhrase());
+        assertNotNull(receivedCat);
+        assertEquals(cat.getName(), receivedCat.getName());
+
+    }
+
+    @Test
+    public void getAllCats() {
+        RestTemplate restTemplate = new RestTemplate();
+        createCat();
+        createCat();
+
+        ResponseEntity<List<Cat>> result = restTemplate.exchange(
+                ROOT + GET_ALL,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Cat>>() {
+                }
+        );
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertNotNull(result.getBody());
+    }
+
+    private Cat createCat() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
+
+        Cat cat = prefillCat();
+        HttpEntity<Cat> httpEntity = new HttpEntity<>(cat, httpHeaders);
+
+        RestTemplate restTemplate = new RestTemplate();
+        Cat createdCat = restTemplate.exchange(
+                ROOT + ADD,
+                HttpMethod.PUT,
+                httpEntity,
+                Cat.class).getBody();
+        assertNotNull(createdCat);
+        assertEquals("Barsik", createdCat.getName());
+        assertEquals("sleepy", createdCat.getDescription());
+        assertNotNull(createdCat.getId());
+
+        return createdCat;
+    }
+
+    private Cat prefillCat() {
+        Cat cat = new Cat();
+        cat.setName("Barsik");
+        cat.setDescription("sleepy");
+        return cat;
+    }
+}

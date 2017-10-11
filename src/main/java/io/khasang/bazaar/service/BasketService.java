085157afbfb82b126d5dfd125f32e24464cbe711
Keos99 <keos99@bk.ru>
2017-10-11 20:29:39 +0300

    Fix.

--- src/main/java/io/khasang/bazaar/service/BasketService.java
+++ src/main/java/io/khasang/bazaar/service/BasketService.java
@@ -6,18 +6,23 @@ import java.util.List;
 
 public interface BasketService {
     /**
-     *
+     * Method for retrieving goods of basket by their database id.
+     * @param id - id of the goods of basket in the database
+     * @return goods of basket with given id
      */
     Basket getBasketById(Long id);
 
     /**
-     *
+     * Method for retrieving goods of basket from the database.
+     * @return all the goods of basket.
      */
 
     List<Basket> getBaskets();
 
     /**
-     *
+     * Method for adding new goods to the database table
+     * @param goods- the object being added to the database table as a row
+     * @return the goods that was added
      */
     Basket addGoodsInBasket (Basket basket);
 

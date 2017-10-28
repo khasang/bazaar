d140cb24d33abbe93e8e3e42c7ac60296ffec261
achubukov <sasha-chubucov@mail.ru>
2017-10-28 23:50:30 +0300

    add task 12 (support) refresh edit SupportServiceImpl

--- src/main/java/io/khasang/bazaar/service/impl/SupportServiceImpl.java
+++ src/main/java/io/khasang/bazaar/service/impl/SupportServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service("deliveryService")
+@Service("supportService")
 public class SupportServiceImpl implements SupportService {
 
     @Autowired
